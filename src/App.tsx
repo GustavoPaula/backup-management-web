34f3d06d151da14693f181b89d75b8706ba07cff
GustavoPaula <gustavosilvadepaula95@outlook.com>
2025-11-16 00:12:26 -0300

    feat: config react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useState, useCallback } from 'react';
 import './App.css';
+import { Router } from './Router';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleCount = useCallback(() => {
-    setCount((count) => count + 1);
-  }, []);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={handleCount}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router />
     </>
   );
 }
